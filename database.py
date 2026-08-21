05dfb705eb946964e367009731caf4f387ba588b
arfinaleusuf <arfinaleusuf@gmail.com>
2026-08-21 14:13:01 +0600

    Update database connection string for Supabase deployment

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password@localhost:5432/ExpenseTrackerAPI'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.htahxzemzfairzpymhzw:ExpenseTrackerAPI@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
